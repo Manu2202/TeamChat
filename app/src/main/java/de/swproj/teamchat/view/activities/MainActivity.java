6cc142e54e4b819c7d0ebcd9aa824706b76602d9
Manu2202 <56685473+manu2202@users.noreply.github.com>
2019-11-14 17:30:27 +0100

    BottomNavView in MainActivity und Fragments

--- app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
+++ app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
@@ -1,24 +1,96 @@
 package de.swproj.teamchat.view.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import de.swproj.teamchat.R;
+import de.swproj.teamchat.view.fragments.FragmentMainChats;
+import de.swproj.teamchat.view.fragments.FragmentMainContacts;
+import de.swproj.teamchat.view.fragments.FragmentMainEvents;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvChat;
     private ListView lvEvents;
+    private ListFragment chatFragment;
+    private ListFragment eventFragment;
+    private ListFragment contactFragment;
+    private static Fragment lastSelectedFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpUI();
     }
 
+
+
+    /*
+     * Method on Click of the Button "New Chat". Open the Activity "EditChatActivity"
+     */
     public void createChat(View view){
 
     }
+
+    /*
+     * Private Method to setup the fragements and the Bottom Navigation View
+     */
+    private void setUpUI(){
+        // Set the BottomNavigationView
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_main);
+        // Initialize the Listfragments only when Activity is created
+        chatFragment = new FragmentMainChats();
+        eventFragment = new FragmentMainEvents();
+        contactFragment = new FragmentMainContacts();
+
+        //Set Up the first Fragment, if lastSelectedFragment is NULL, else use this
+        if(lastSelectedFragment == null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_main, chatFragment).commit();
+        }else{
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_main, lastSelectedFragment).commit();
+        }
+
+
+        // Set Up the Listener on the bottomNav
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch(menuItem.getItemId()) {
+                    case R.id.nav_chats:
+                        selectedFragment = chatFragment;
+                        break;
+
+                    case R.id.nav_events:
+                        selectedFragment = eventFragment;
+                        break;
+
+                    case R.id.nav_contacts:
+                        selectedFragment = contactFragment;
+                        break;
+                }
+                lastSelectedFragment = selectedFragment;
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container_main, selectedFragment).commit();
+
+            return true;
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/de/swproj/teamchat/view/fragments/FragmentMainContacts.java
@@ -0,0 +1,47 @@
+package de.swproj.teamchat.view.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+
+/*
+ * Created by Manuel Lanzinger on 14. November 2019.
+ * For the project: TeamChat.
+ */
+
+public class FragmentMainContacts extends ListFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //TODO: Den Adapter erzeugen und mit Werten füllen und auf ListFragment setzen
+        Log.d("Fragments:", "In Contact Fragment");
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView list = getListView();
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //TODO: Konfiguration des Click Listener auf der ListView
+            }
+        });
+    }
+}
