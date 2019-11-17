3ca7c6bb7ad4cde6cf9a8606020d151050ae19ce
Nikolas <orlowitsch@mail.hs-ulm.de>
2019-11-17 15:45:59 +0100

    Chat/Event Adapter

--- app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
+++ app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
@@ -1,12 +1,20 @@
 package de.swproj.teamchat.view.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
+import de.swproj.teamchat.Connection.Database.DBStatements;
+import de.swproj.teamchat.R;
 import de.swproj.teamchat.datamodell.chat.Chat;
+import de.swproj.teamchat.datamodell.chat.Message;
 
 
 /*
@@ -17,28 +25,66 @@ import de.swproj.teamchat.datamodell.chat.Chat;
 public class AdapterChat extends BaseAdapter {
 
     private ArrayList<Chat> chats;
+    private DBStatements db;
+    private AppCompatActivity activity;
 
-    public AdapterChat(ArrayList<Chat> chats) {
+    public AdapterChat(ArrayList<Chat> chats, DBStatements dbStatements, AppCompatActivity activity) {
         this.chats = chats;
+        db = dbStatements;
+        this.activity = activity;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return chats.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Chat getItem(int position) {
+        return chats.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        Context context = parent.getContext();
+        final Chat chat = chats.get(position);
+
+        if (convertView == null) {
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listitem_chat, null, false);
+
+            // Color
+            View colorBox = convertView.findViewById(R.id.list_color_box);
+            colorBox.setBackgroundColor(chat.getColor());
+
+            // Name
+            TextView chatName = (TextView) convertView.findViewById(R.id.chatListChatName);
+            chatName.setText(chat.getName());
+
+            // Last message
+            TextView lastMessage = (TextView) convertView.findViewById(R.id.chatListLastMessage);
+            final Message lastMsg = db.getLastMessage(chat.getId());
+
+
+            // Avoid NullpointerException if Chat is empty
+            if (lastMsg == null) {
+            } else {
+                lastMessage.setText(lastMsg.getMessage());
+
+                // Date
+                TextView messageDate = (TextView) convertView.findViewById(R.id.chatListLastMessageDate);
+                messageDate.setText(lastMsg.getTimeStamp().toString());
+            }
+
+
+        }
+
+        return convertView;
     }
 }
