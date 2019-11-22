6704a4f5ec3667957ec32342eef781e3d3b7a098
orlowitsch <orlowitsch@mail.hs-ulm.de>
2019-11-22 15:12:14 +0100

    Adapter eingebunden

--- app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
+++ app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
@@ -1,6 +1,7 @@
 package de.swproj.teamchat.view.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,12 +27,17 @@ public class AdapterChat extends BaseAdapter {
 
     private ArrayList<Chat> chats;
     private DBStatements db;
-    private AppCompatActivity activity;
+    private boolean darkColor = false;
 
-    public AdapterChat(ArrayList<Chat> chats, DBStatements dbStatements, AppCompatActivity activity) {
+    // Set to false if you want Chat List to have white background + black text
+    // Set to true if you want colored background (with white text, if chatColor is too dark)
+    private boolean coloredBackground = false;
+
+
+    public AdapterChat(ArrayList<Chat> chats, DBStatements dbStatements) {
         this.chats = chats;
         db = dbStatements;
-        this.activity = activity;
+
     }
 
     @Override
@@ -54,7 +60,7 @@ public class AdapterChat extends BaseAdapter {
         Context context = parent.getContext();
         final Chat chat = chats.get(position);
 
-        if (convertView == null) {
+    //    if (convertView == null) {
 
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listitem_chat, null, false);
@@ -63,27 +69,62 @@ public class AdapterChat extends BaseAdapter {
             View colorBox = convertView.findViewById(R.id.list_color_box);
             colorBox.setBackgroundColor(chat.getColor());
 
+            // Colored Background
+        View colorBackground = convertView.findViewById(R.id.list_color_background);
+        if (coloredBackground) {
+            int red = Color.red(chat.getColor());
+            int green = Color.green(chat.getColor());
+            int blue = Color.blue(chat.getColor());
+            float[] hsv = new float[3];
+            Color.RGBToHSV(red, green, blue, hsv);
+            // reduce saturation to 20%
+            hsv[1] = hsv[1] * 0.20f;
+            if (hsv[2] < 0.66f) {
+                darkColor = true;
+                hsv[2] = 0.2f;
+            } else
+                darkColor = false;
+            colorBackground.setBackgroundColor(Color.HSVToColor(hsv));
+            // Check if the color is too dark for black text
+        }
+        else {
+            colorBackground.setBackgroundColor(0xFFFFFFFF);
+            darkColor = false;
+        }
+
+
             // Name
             TextView chatName = (TextView) convertView.findViewById(R.id.chatListChatName);
             chatName.setText(chat.getName());
+            // Set textcolor to white if the color of the background is too dark
+            if (darkColor)
+                chatName.setTextColor(0xFFCAC8CA);
 
             // Last message
             TextView lastMessage = (TextView) convertView.findViewById(R.id.chatListLastMessage);
             final Message lastMsg = db.getLastMessage(chat.getId());
+            if (darkColor)
+                lastMessage.setTextColor(0xFFFFFFFF);
 
 
             // Avoid NullpointerException if Chat is empty
             if (lastMsg == null) {
+                TextView messageDate = (TextView) convertView.findViewById(R.id.chatListLastMessageDate);
+                messageDate.setText("10.10.2025");
+                if (darkColor)
+                    messageDate.setTextColor(0xFFCAC8CA);
             } else {
                 lastMessage.setText(lastMsg.getMessage());
 
                 // Date
                 TextView messageDate = (TextView) convertView.findViewById(R.id.chatListLastMessageDate);
                 messageDate.setText(lastMsg.getTimeStamp().toString());
+                if (darkColor)
+                    messageDate.setTextColor(0xFFCAC8CA);
             }
 
 
-        }
+      //  }
 
         return convertView;
     }
--- app/src/main/java/de/swproj/teamchat/view/fragments/FragmentMainChats.java
+++ app/src/main/java/de/swproj/teamchat/view/fragments/FragmentMainChats.java
@@ -1,5 +1,6 @@
 package de.swproj.teamchat.view.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,7 +13,14 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
+
+import java.util.ArrayList;
+
+import de.swproj.teamchat.Connection.database.DBStatements;
 import de.swproj.teamchat.R;
+import de.swproj.teamchat.datamodell.chat.Chat;
+import de.swproj.teamchat.view.activities.ChatActivity;
+import de.swproj.teamchat.view.adapter.AdapterChat;
 
 
 /*
@@ -37,15 +45,42 @@ public class FragmentMainChats extends ListFragment {
         super.onActivityCreated(savedInstanceState);
 
         ListView list = getListView();
-        String[] s = new String[]{"Hier könnte Ihre Werbung stehen!", "Wählen Sie die 62 70 8"};
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.dummy_list_item, R.id.textView, s);
-        setListAdapter(adapter);
+     //   String[] s = new String[]{"Hier könnte Ihre Werbung stehen!", "Wählen Sie die 62 70 8"};
+     //   ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.dummy_list_item, R.id.textView, s);
+        //  setListAdapter(adapter);
 
+        DBStatements db = new DBStatements(getContext());
+
+
+        // Dummy Data
+        ArrayList<Chat> chats = new ArrayList<Chat>();
+        chats.add(new Chat("Labergruppe", 0xFFFB0B03, "394", "Gott"));
+        chats.add(new Chat("Tetris esport Team", 0xFFFBB400, "3934", "Gott"));
+        chats.add(new Chat("Anonyme Alkoholiker", 0xFFB0FB03, "3954", "Gott"));
+        chats.add(new Chat("Öffentliche Alkoholiker", 0xFF00FB71, "3941", "Gott"));
+        chats.add(new Chat("Saufgruppe 1", 0xFF0C00F1, "3434", "Gott"));
+        chats.add(new Chat("Saufgruppe 2", 0xFF038814, "3414", "Gott"));
+        chats.add(new Chat("Saufgruppe 3", 0xFF880E51, "3484", "Gott"));
+        chats.add(new Chat("Saufgruppe 4", 0xFF884318, "3214", "Gott"));
+        chats.add(new Chat("Saufgruppe 5", 0xFF004888, "3474", "Gott"));
+        chats.add(new Chat("Saufgruppe 6", 0xFF880B00, "3414", "Gott"));
+        chats.add(new Chat("Saufgruppe 7", 0xFF88004D, "3484", "Gott"));
+        chats.add(new Chat("Saufgruppe 8", 0xFF888800, "3214", "Gott"));
+        chats.add(new Chat("Saufgruppe 9", 0xFF7F0088, "3474", "Gott"));
+
+
+
+        final AdapterChat chatAdapter = new AdapterChat(chats, db);
+        setListAdapter(chatAdapter);
+
+//TODO: Test ClickListener once ChatActivity can be tested
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //TODO: Konfiguration des Click Listener auf der ListView
+                Intent chatIntent = new Intent(getActivity(), ChatActivity.class);
+                Chat selectedItem = (Chat)chatAdapter.getItem(position);
+                chatIntent.putExtra("chatID", selectedItem.getId());
+                startActivityForResult(chatIntent, position);
             }
         });
     }
