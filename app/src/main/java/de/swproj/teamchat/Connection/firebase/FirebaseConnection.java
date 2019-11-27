f31487d70f0f8329e5442023aaad218b745fae4e
Manu2202 <56685473+Manu2202@users.noreply.github.com>
2019-11-27 10:14:26 +0100

    Eigene FirebaseConnection Klasse und Edit Klassen Speicherung angepasst

---
+++ app/src/main/java/de/swproj/teamchat/Connection/firebase/FirebaseConnection.java
@@ -0,0 +1,43 @@
+package de.swproj.teamchat.Connection.firebase;
+
+/*
+ * Created by Manuel Lanzinger on 27. November 2019.
+ * For the project: TeamChat.
+ */
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+import androidx.annotation.NonNull;
+
+public class FirebaseConnection {
+
+    private FirebaseFirestore firebaseDB;
+    private String objectID;
+
+    public void FirebaseConnection(){
+        // Connect Firebase
+        firebaseDB = FirebaseFirestore.getInstance();
+    }
+
+    public String addToFirestore(String collectionPath, HashMap<String, Object> objectData){
+        firebaseDB.collection(collectionPath).add(objectData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                objectID = documentReference.getId();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                objectID = "-1";
+            }
+        });
+
+        // Return the Object ID of the entry of Firestore
+        return objectID;
+    }
+}
