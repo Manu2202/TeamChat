4c0de700519fbcad57077e1a0667679085644d1d
orlowitsch <orlowitsch@mail.hs-ulm.de>
2019-12-04 12:04:59 +0100

    Gruppe erstellen

--- app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
+++ app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private void  addTestdat(){
 
         //löscht alle eintäge beim Start
-        db.dropAll();
+        //db.dropAll();
 
         db.insertUser(new User("Gott","sdbjhdj","Der Herr der Dinge","Gott","Herr"));
         db.insertUser(new User("11","sdjhnjhdj","Horster","Hors","tidiot"));
@@ -226,5 +226,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 }
