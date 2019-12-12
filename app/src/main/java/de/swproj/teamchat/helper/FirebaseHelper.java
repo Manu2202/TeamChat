1b066562c570af7f7c97cd2d3cd0ca09cbb0de26
Manu2202 <56685473+Manu2202@users.noreply.github.com>
2019-12-12 18:04:15 +0100

    Reformat Code und Kommentare

--- app/src/main/java/de/swproj/teamchat/helper/FirebaseHelper.java
+++ app/src/main/java/de/swproj/teamchat/helper/FirebaseHelper.java
@@ -7,19 +7,23 @@ import de.swproj.teamchat.datamodell.chat.Message;
 
 public class FirebaseHelper {
 
-public static HashMap<String,String> convertToMap(Message message){
-    HashMap<String,String> retMap= new HashMap<>();
-    if(message.isEvent()){
-        Event event = (Event)message;
-        retMap.put("date",FormatHelper.formatDate(event.getDate()));
-        retMap.put("description",event.getDescription());
-        retMap.put("status",String.valueOf(event.getStatus()));
+    /*
+     * Static Helper Method to put all Data in Strings
+     * -> need it to send the class via Firebase (only accepting Strings)
+     */
+    public static HashMap<String, String> convertToMap(Message message) {
+        HashMap<String, String> retMap = new HashMap<>();
+        if (message.isEvent()) {
+            Event event = (Event) message;
+            retMap.put("date", FormatHelper.formatDate(event.getDate()));
+            retMap.put("description", event.getDescription());
+            retMap.put("status", String.valueOf(event.getStatus()));
+        }
+        retMap.put("chatid", message.getChatid());
+        retMap.put("creator", message.getCreator());
+        retMap.put("message", message.getMessage());
+        retMap.put("isEvent", ((Boolean) message.isEvent()).toString());
+        retMap.put("timestamp", FormatHelper.formatTime(message.getTimeStamp()));
+        return retMap;
     }
-    retMap.put("chatid",message.getChatid());
-    retMap.put("creator",message.getCreator());
-    retMap.put("message",message.getMessage());
-    retMap.put("isEvent",((Boolean)message.isEvent()).toString());
-    retMap.put("timestamp",FormatHelper.formatTime(message.getTimeStamp()));
-    return retMap;
-}
 }
