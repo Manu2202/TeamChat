185421aa8b199b631972662d1588dec96da89f61
Manu2202 <56685473+Manu2202@users.noreply.github.com>
2019-12-12 17:18:32 +0100

    Jedes Fragment eigenes Menü

--- app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
+++ app/src/main/java/de/swproj/teamchat/view/activities/MainActivity.java
@@ -45,22 +45,22 @@ public class MainActivity extends AppCompatActivity {
     //FirebaseAuth
     private FirebaseAuth mAuth;
 
-    private void  addTestdat(){
+    private void addTestdat() {
 
         //löscht alle eintäge beim Start
         db.dropAll();
 
-        db.insertUser(new User("Gott","sdbjhdj","Der Herr der Dinge","Gott","Herr"));
-        db.insertUser(new User("11","sdjhnjhdj","Horster","Hors","tidiot"));
-        db.insertUser(new User("abc","sdjjunhdj","ICH","Man","Derine"));
-        db.insertUser(new User("Gott2","sdbjhdj","Der Herr der Dinge2","Gott2","Herr"));
-        db.insertUser(new User("emusk","sdbf","Elon Musk","Musk","Elon"));
-        db.insertChat(new Chat("Gugel",(getResources().getIntArray(R.array.androidcolors))[0],"123","11"));
-        db.updateChatMembers(new String[]{"Gott","11","abc","Gott2"},"123");
-        db.updateChatMembers(new String[]{"Gott","11","abc","Gott2"},"3434");
+        db.insertUser(new User("Gott", "sdbjhdj", "Der Herr der Dinge", "Gott", "Herr"));
+        db.insertUser(new User("11", "sdjhnjhdj", "Horster", "Hors", "tidiot"));
+        db.insertUser(new User("abc", "sdjjunhdj", "ICH", "Man", "Derine"));
+        db.insertUser(new User("Gott2", "sdbjhdj", "Der Herr der Dinge2", "Gott2", "Herr"));
+        db.insertUser(new User("emusk", "sdbf", "Elon Musk", "Musk", "Elon"));
+        db.insertChat(new Chat("Gugel", (getResources().getIntArray(R.array.androidcolors))[0], "123", "11"));
+        db.updateChatMembers(new String[]{"Gott", "11", "abc", "Gott2"}, "123");
+        db.updateChatMembers(new String[]{"Gott", "11", "abc", "Gott2"}, "3434");
 
 
-      //  db.insertChat(new Chat("Labergruppe", 0xFFFB0B03, "394", "Gott"));
+        //  db.insertChat(new Chat("Labergruppe", 0xFFFB0B03, "394", "Gott"));
         db.insertChat(new Chat("Tetris esport Team", (getResources().getIntArray(R.array.androidcolors))[1], "3934", "Gott"));
         db.insertChat(new Chat("Anonyme Alkoholiker", (getResources().getIntArray(R.array.androidcolors))[2], "3954", "Gott"));
         db.insertChat(new Chat("Öffentliche Alkoholiker", (getResources().getIntArray(R.array.androidcolors))[3], "3941", "Gott"));
@@ -73,31 +73,31 @@ public class MainActivity extends AppCompatActivity {
         Date currentTime = Calendar.getInstance().getTime();
         GregorianCalendar gc = new GregorianCalendar();
         gc.setGregorianChange(new java.sql.Date(849494994));
-             Time time =new Time (currentTime.getTime());
-        db.insertMessage(new Message(time,"Hallo, der horst ist da!!!","oho",false,"11","123"));
-        time.setTime(currentTime.getTime()+5);
-        db.insertMessage(new Event(time,"Panik","546s",true,"11",gc,"hilfe ein virus","123",(byte)1));
-        time.setTime(currentTime.getTime()+10);
-        db.insertMessage(new Message(time,"Coolbbb bfgtf ;D","o4454546",false,"abc","123"));
-        time.setTime(currentTime.getTime()+10);
-        db.insertMessage(new Message(time,"Cool  hu;D","o4584846",false,"11","123"));
-        time.setTime(currentTime.getTime()+10);
-        db.insertMessage(new Message(time,"Cool  huhu;D","o448784546",false,"Gott2","123"));
-        time.setTime(currentTime.getTime()+10);
-        db.insertMessage(new Message(time,"Cool ;D","o454846",false,"Gott","123"));
-        time.setTime(currentTime.getTime()+10);
-        db.insertMessage(new Message(time,"Ein Huhun ;D","o4115jn546",false,"abc","123"));
+        Time time = new Time(currentTime.getTime());
+        db.insertMessage(new Message(time, "Hallo, der horst ist da!!!", "oho", false, "11", "123"));
+        time.setTime(currentTime.getTime() + 5);
+        db.insertMessage(new Event(time, "Panik", "546s", true, "11", gc, "hilfe ein virus", "123", (byte) 1));
+        time.setTime(currentTime.getTime() + 10);
+        db.insertMessage(new Message(time, "Coolbbb bfgtf ;D", "o4454546", false, "abc", "123"));
+        time.setTime(currentTime.getTime() + 10);
+        db.insertMessage(new Message(time, "Cool  hu;D", "o4584846", false, "11", "123"));
+        time.setTime(currentTime.getTime() + 10);
+        db.insertMessage(new Message(time, "Cool  huhu;D", "o448784546", false, "Gott2", "123"));
+        time.setTime(currentTime.getTime() + 10);
+        db.insertMessage(new Message(time, "Cool ;D", "o454846", false, "Gott", "123"));
+        time.setTime(currentTime.getTime() + 10);
+        db.insertMessage(new Message(time, "Ein Huhun ;D", "o4115jn546", false, "abc", "123"));
 
-        db.insertMessage(new Event(time,"TourdeFrance","4546s",true,"Gott", new GregorianCalendar(2020, 10, 27, 9, 6),"hilfe ein russ","3434",(byte)1));
-        time.setTime(currentTime.getTime()+10);
+        db.insertMessage(new Event(time, "TourdeFrance", "4546s", true, "Gott", new GregorianCalendar(2020, 10, 27, 9, 6), "hilfe ein russ", "3434", (byte) 1));
+        time.setTime(currentTime.getTime() + 10);
 
-        db.insertMessage(new Event(time,"Mars Tour","14546s",true,"emusk", new GregorianCalendar(2020, 10, 27, 9, 6),"colonize mars with me","123",(byte)1));
-        time.setTime(currentTime.getTime()+1555);
+        db.insertMessage(new Event(time, "Mars Tour", "14546s", true, "emusk", new GregorianCalendar(2020, 10, 27, 9, 6), "colonize mars with me", "123", (byte) 1));
+        time.setTime(currentTime.getTime() + 1555);
 
 
-        Log.d("Main TestDaten  ",db.getUser().size()+"");
-        for(User user:db.getUser()){
-            Log.d("User: ",user.getAccountName()+"");
+        Log.d("Main TestDaten  ", db.getUser().size() + "");
+        for (User user : db.getUser()) {
+            Log.d("User: ", user.getAccountName() + "");
         }
 
     }
@@ -112,67 +112,32 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         db = new DBStatements(this);
     }
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         final FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null){
-            Log.d("User-Problem","User is NULL going back to Start");
+        if (currentUser == null) {
+            Log.d("User-Problem", "User is NULL going back to Start");
             //TeamChatMessagingService.disableFCM();
-            sendtoStart();
+            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+            finish();
 
-        }
-        else {
+        } else {
             TeamChatMessagingService.enableFCM();
-            Log.d("User-Problem", "Logged in as User"+ currentUser.getDisplayName()+ " with UID of:"+ currentUser.getUid());
+            Log.d("User-Problem", "Logged in as User" + currentUser.getDisplayName() + " with UID of:" + currentUser.getUid());
             setUpUI();
         }
 
-    }
-    private void sendtoStart() {
-        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-        startActivity(startIntent);
-        finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        switch(item.getItemId()){
-            case R.id.btn_main_logout:
-                //delete Token from uID
-                if (FirebaseAuth.getInstance().getCurrentUser()!=null){
-                    FirebaseConnection.deleteToken(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                }
-                TeamChatMessagingService.disableFCM();
-                FirebaseAuth.getInstance().signOut();
-                sendtoStart();
-                break;
-
-            case R.id.btn_main_new_chat:
-                Intent createChatIntent = new Intent(this, EditChatActivity.class);
-                // ID = 0 -> new Chat
-                createChatIntent.putExtra("ID", "0");
-                startActivity(createChatIntent);
-                break;
-        }
-
-        return true;
     }
 
 
     /*
      * Private Method to setup the fragements and the Bottom Navigation View
      */
-    private void setUpUI(){
+    private void setUpUI() {
         // Set the BottomNavigationView
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_main);
         // Initialize the Listfragments only when Activity is created
@@ -185,12 +150,11 @@ public class MainActivity extends AppCompatActivity {
         //db.dropAll();
 
 
-
         //Set Up the first Fragment, if lastSelectedFragment is NULL, else use this
-        if(lastSelectedFragment == null){
+        if (lastSelectedFragment == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container_main, chatFragment).commit();
-        }else{
+        } else {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container_main, lastSelectedFragment).commit();
         }
@@ -202,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 Fragment selectedFragment = null;
 
-                switch(menuItem.getItemId()) {
+                switch (menuItem.getItemId()) {
                     case R.id.nav_chats:
                         selectedFragment = chatFragment;
                         break;
@@ -220,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container_main, selectedFragment).commit();
 
-            return true;
+                return true;
             }
         });
     }
--- app/src/main/java/de/swproj/teamchat/view/fragments/FragmentMainEvents.java
+++ app/src/main/java/de/swproj/teamchat/view/fragments/FragmentMainEvents.java
@@ -5,10 +5,15 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -16,8 +21,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
+import de.swproj.teamchat.R;
 import de.swproj.teamchat.connection.database.DBStatements;
+import de.swproj.teamchat.connection.firebase.FirebaseConnection;
+import de.swproj.teamchat.connection.firebase.services.TeamChatMessagingService;
 import de.swproj.teamchat.datamodell.chat.Event;
+import de.swproj.teamchat.view.activities.StartActivity;
 import de.swproj.teamchat.view.activities.ViewEventActivity;
 import de.swproj.teamchat.view.adapter.AdapterEvent;
 
@@ -56,6 +65,8 @@ public class FragmentMainEvents extends ListFragment {
         //list.setDividerHeight(10);
         setListAdapter(adapterEvent);
 
+        setHasOptionsMenu(true);
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -65,5 +76,30 @@ public class FragmentMainEvents extends ListFragment {
                 startActivityForResult(eventIntent, position);
             }
         });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_event_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.btn_main_event_logout:
+                //delete Token from uID
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    FirebaseConnection.deleteToken(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                }
+                TeamChatMessagingService.disableFCM();
+                FirebaseAuth.getInstance().signOut();
+                Intent startIntent = new Intent(getActivity(), StartActivity.class);
+                startActivity(startIntent);
+                getActivity().finish();
+                break;
+        }
+        return true;
     }
 }
