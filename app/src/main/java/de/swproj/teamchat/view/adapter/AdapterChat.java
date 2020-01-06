c849248045349085e0a4a3b32ccd8061495279fb
Manu2202 <56685473+Manu2202@users.noreply.github.com>
2020-01-06 14:40:11 +0100

    Übersicht Chat: Vor letzer Message anzeige den Absender eingefügt + Bugfix bei Anmeldung

--- app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
+++ app/src/main/java/de/swproj/teamchat/view/adapter/AdapterChat.java
@@ -14,6 +14,7 @@ import de.swproj.teamchat.connection.database.DBStatements;
 import de.swproj.teamchat.R;
 import de.swproj.teamchat.datamodell.chat.Chat;
 import de.swproj.teamchat.datamodell.chat.Message;
+import de.swproj.teamchat.datamodell.chat.User;
 
 
 /*
@@ -70,7 +71,8 @@ public class AdapterChat extends BaseAdapter {
 
         // Avoid NullpointerException if Chat is empty
         if (lastMsg != null) {
-            lastMessage.setText(lastMsg.getMessage());
+            User msgSender = DBStatements.getUser(lastMsg.getCreator());
+            lastMessage.setText(msgSender.getFirstName() + ": " + lastMsg.getMessage());
 
             // Date
             TextView messageDate = (TextView) convertView.findViewById(R.id.chatListLastMessageDate);
