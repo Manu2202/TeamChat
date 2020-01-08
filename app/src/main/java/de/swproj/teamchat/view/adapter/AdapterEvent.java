00c0f7c9b9169672f5af538515beedba8575b7cf
orlowitsch <orlowitsch@mail.hs-ulm.de>
2020-01-08 18:03:36 +0100

    Cancelled Events in Chat now greyed out (User Event Status not correct yet)

--- app/src/main/java/de/swproj/teamchat/view/adapter/AdapterEvent.java
+++ app/src/main/java/de/swproj/teamchat/view/adapter/AdapterEvent.java
@@ -155,6 +155,8 @@ public class AdapterEvent extends BaseAdapter {
                  }
              }
 
+
+
              // Display name of group this event belongs to (this is the event overview, so
              // it makes more sense here to display the group name instead of the event creator name)
              tvGroupname.setText(DBStatements.getChat(ev.getChatid()).getName());
