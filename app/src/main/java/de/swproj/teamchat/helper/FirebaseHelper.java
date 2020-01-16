6bd8d7e61d4760066a04bef6acbc4b6d23867fdc
Fabi98 <fabian.dittrich98@gmail.com>
2020-01-16 17:51:31 +0100

    - eventname fixed

--- app/src/main/java/de/swproj/teamchat/helper/FirebaseHelper.java
+++ app/src/main/java/de/swproj/teamchat/helper/FirebaseHelper.java
@@ -53,7 +53,9 @@ public static HashMap<String,String> convertToMap(Message message, int type, int
         retMap.put("reason",status.getReason());
         retMap.put("status",status.getStatus());
         retMap.put("userid",status.getUserId());
-        retMap.put("eventname",DBStatements.getEvent(status.getEventId()).getMessage());
+        Event event = DBStatements.getEvent(status.getEventId());
+        retMap.put("chatid",event.getChatid());
+        retMap.put("eventname",event.getMessage());
         retMap.put("notification","User"+ DBStatements.getUser(status.getUserId()).getAccountName()+ " "+status.getStatusString());
         retMap.put("type",String.valueOf(type));
         retMap.put("action",String.valueOf(action));
