0c65cfde2388f40050f89ea3370c18500054e08d
orlowitsch <orlowitsch@mail.hs-ulm.de>
2020-01-18 13:58:06 +0100

    Fixed local DB missing current user

--- app/src/main/java/de/swproj/teamchat/view/activities/LoginActivity.java
+++ app/src/main/java/de/swproj/teamchat/view/activities/LoginActivity.java
@@ -15,12 +15,21 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 import de.swproj.teamchat.R;
+import de.swproj.teamchat.connection.database.DBStatements;
 import de.swproj.teamchat.connection.firebase.FirebaseConnection;
 import de.swproj.teamchat.datamodell.chat.User;
 
@@ -32,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     //Get shared Preference File Key
     public static final String PREFERENCE_FILE_KEY = "FirebasePref_Token";
-    public static final String KEY="Token";
+    public static final String KEY = "Token";
 
 
     @Override
@@ -43,27 +52,28 @@ public class LoginActivity extends AppCompatActivity {
         //FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
 
-        mLoginProgress=new ProgressDialog(this);
-        mLoginEmail = (TextInputLayout)findViewById(R.id.login_email);
-        mLoginPassword = (TextInputLayout)findViewById(R.id.login_password);
-        mloginButton = (Button)findViewById(R.id.btn_login_login);
+        mLoginProgress = new ProgressDialog(this);
+        mLoginEmail = (TextInputLayout) findViewById(R.id.login_email);
+        mLoginPassword = (TextInputLayout) findViewById(R.id.login_password);
+        mloginButton = (Button) findViewById(R.id.btn_login_login);
         mloginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email= mLoginEmail.getEditText().getText().toString();
+                String email = mLoginEmail.getEditText().getText().toString();
                 String password = mLoginPassword.getEditText().getText().toString();
-                if (!TextUtils.isEmpty(email)||!TextUtils.isEmpty(password)){
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
                     mLoginProgress.setMessage("Please wait while we check your credentials");
                     mLoginProgress.setTitle("Logging in");
                     mLoginProgress.setCanceledOnTouchOutside(false);
                     mLoginProgress.show();
-                    loginUser(email,password);
+                    loginUser(email, password);
                 }
             }
         });
     }
 
     private void loginUser(String email, String password) {
+        final String checkEmail = email;
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -71,10 +81,15 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             mLoginProgress.dismiss();
                             //Assign Token from Shared Preference
-                            String token=get_token();
-                            FirebaseConnection.updateToken(FirebaseAuth.getInstance().getCurrentUser().getUid(),token);
+                            String token = get_token();
+
+                            if (!DBStatements.getUserEmailExists(checkEmail)) {
+                                queryFirebaseByID(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            }
+
+                            FirebaseConnection.updateToken(FirebaseAuth.getInstance().getCurrentUser().getUid(), token);
                             Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             startActivity(mainIntent);
                             finish();
                         } else {
@@ -87,11 +102,44 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
-    private String get_token(){
+
+    private String get_token() {
         SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCE_FILE_KEY, MODE_PRIVATE);
-        return sharedPreferences.getString("Token","--noTokenAvailable--");
+        return sharedPreferences.getString("Token", "--noTokenAvailable--");
 
     }
+
+
+    private void queryFirebaseByID(String id) {
+        //Check if Email exists for User in Auth------------------------------------------
+        final String fireBaseID = id;
+
+        final User user;
+
+        //Get User from Firestore (gets saved in Database)
+        FirebaseFirestore.getInstance().collection("users").whereEqualTo("googleId", fireBaseID).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult() != null) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            //Got User, anything related to this User has to be done inside here because its async ------->
+                            User firebaseUser = document.toObject(User.class);
+                            Log.d("FirebaseUser", firebaseUser.getAccountName() + ", " + firebaseUser.getFirstName());
+                            DBStatements.insertUser(firebaseUser);
+
+                        }
+                    }
+
+                    }
+
+            }
+        });
+
+    }
+
+
+
 }
 
 
